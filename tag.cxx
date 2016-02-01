ffba9c1edde9726c796e3bf70dc1b31e9bb6c542
Kevin Heinicke <kevin@kehei.de>
2016-02-01 18:25:24 +0100

    predict!

--- tag.cxx
+++ tag.cxx
@@ -3,18 +3,34 @@
 
 #include <cmath>
 #include <vector>
+#include <array>
 #include <iostream>
 #include "xgboost/c_api.h"
 
 int main(int argc, char* argv[]){
   // TODO: read a root file here and build a xgboost compatible DMatrix
-  auto *mat = DMatrixHandle();
-  XGDMatrixCreateFromFile("../data/nnet_ele.xmat", 1, &mat);
+  DMatrixHandle mat;
+  XGDMatrixCreateFromFile("../data/nnet_ele.xmat", 0, &mat);
 
   unsigned long num_rows;
   XGDMatrixNumRow(mat, &num_rows);
 
   std::cout << "File contains " << num_rows << " rows." << std::endl;
+
+  DMatrixHandle mat_array[] = {mat};
+
+  BoosterHandle booster;
+  XGBoosterCreate(mat_array, 1, &booster);
+  std::cout << "Loading model "
+    << XGBoosterLoadModel(booster, "../models/ele_trained.xgb")
+    << std::endl;
+
+  std::vector<const float*> predictions(num_rows);
+  XGBoosterPredict(booster, mat, 0, 0, &num_rows, &predictions[0]);
+
+  for(auto *p : predictions){
+    std::cout << p << std::endl;
+  }
 
   // use the model to predict something
   // auto *predictions = bst->Pred(*mat, 0x00, 0, len);
