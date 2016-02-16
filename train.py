a0829eaa1a17978f0b90fed2e9139ba6be760aae
Kevin Heinicke <kevin@kehei.de>
2016-02-17 00:10:39 +0100

    try to use own dataset

--- train.py
+++ train.py
@@ -12,18 +12,16 @@ from uncertainties import ufloat
 import sys
 import os
 
-from scripts.utils import get_events_statistics
-
 # Some constant definitions
 # TODO: handle with configargparse?
-E_DATA_PATH = os.path.abspath('./data/nnet_ele.root')
-FEATURES = ['mult',
-            'partPt',
+E_DATA_PATH = os.path.abspath('./data/out.root')
+FEATURES = ['partPt',
             'partP',
-            'ptB',
-            'IPs',
+            'BPt',
+            # Ignore IPs because of lacking preselection implementation (?)
+            # 'IPs',
             'partlcs',
-            'eOverP',
+            'PIDNNm',
             'ghostProb',
             'IPPU']
 SWEIGHT_THRESHOLD = 0.
@@ -34,28 +32,13 @@ data = read_root(E_DATA_PATH)
 print(' done.')
 
 # prepare dataset
-data['label'] = data.iscorrect
-data['event_id'] = data.runNum.apply(str) + '_' + data.evtNum.apply(str)
+data['label'] = (data.trueTag + 1) / 2
+# data['event_id'] = data.runNum.apply(str) + '_' + data.evtNum.apply(str)
 
 # Check for nonempty dataset
 assert not data[FEATURES].empty, 'FEATURE selection wrong, or dataset empty'
 
-print("""    File `{}`
-    containing {} tracks and {} events.""".format(
-    E_DATA_PATH,
-    get_events_statistics(data)['tracks'],
-    get_events_statistics(data)['Events'])
-)
-
-cut = data.N_sig_sw > SWEIGHT_THRESHOLD
-
-print("""Cutting sweight {}
-    {} tracks passing
-    {} tracks lost""".format(
-    SWEIGHT_THRESHOLD,
-    get_events_statistics(data[cut])['tracks'],
-    get_events_statistics(data[~cut])['tracks'],
-))
+cut = (data == data).partP
 
 # Build the tree, prepare input data, define xgb parameters
 print('Building model...', end='')
@@ -65,20 +48,20 @@ mask = np.random.rand(len(data)) < 0.7
 
 xgb_training_data = xgb.DMatrix(data[cut & mask].loc[:, FEATURES],
                                 label=data[cut & mask]['label'],
-                                weight=data[cut & mask]['N_sig_sw'],
+                                # weight=data[cut & mask]['N_sig_sw'],
                                 missing=-999.0)
 xgb_test_data = xgb.DMatrix(data[cut & ~mask].loc[:, FEATURES],
                             label=data[cut & ~mask]['label'],
-                            weight=data[cut & ~mask]['N_sig_sw'],
+                            # weight=data[cut & ~mask]['N_sig_sw'],
                             missing=-999.0)
 xgb_full_data = xgb.DMatrix(data[cut].loc[:, FEATURES],
                             label=data[cut]['label'],
-                            weight=data[cut]['N_sig_sw'],
+                            # weight=data[cut]['N_sig_sw'],
                             missing=-999.0)
 
 # store the full dataset in xgboost binary format
 # TODO: Move this into cpp
-xgb_full_data.save_binary("data/nnet_ele.xmat")
+xgb_full_data.save_binary("data/out.xmat")
 evaluation_list = [
     (xgb_test_data, 'eval'),
     (xgb_training_data, 'train'),
@@ -106,7 +89,7 @@ print('Start cross-validation...', end='')
 sys.stdout.flush()
 bst.cv = xgb.cv(parameters,
                 xgb_full_data,
-                nfold=30,
+                nfold=5,
                 show_progress=False,
                 metrics={'auc', 'error'})
 print(' done.')
@@ -117,6 +100,7 @@ last_auc = ufloat(bst.cv['test-auc-mean'].values[max_ind],
                   bst.cv['test-auc-std'].values[max_ind])
 print('Max AUC: {} at {}'.format(last_auc, max_ind))
 
-print('Saving model to `models/ele_trained.xgb`...', end='')
-bst.save_model('models/ele_trained.xgb')
+OUTPUT_FILE = 'out.xgb'
+print('Saving model to `models/{}`...'.format(OUTPUT_FILE), end='')
+bst.save_model('models/{}'.format(OUTPUT_FILE))
 print(' done.')
