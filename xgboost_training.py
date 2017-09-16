7835524d45288c358837a28b122ef77f542c6710
Kevin Heinicke <kevin.heinicke@tu-dortmund.de>
2017-09-16 11:37:58 +0200

    Pickle model and write xgboost weights into textfile
    The save_model method seems to be useless if tried to be combined with
    sklearn

--- xgboost_training.py
+++ xgboost_training.py
@@ -3,8 +3,10 @@
 # since there's still py2 around
 from __future__ import division, print_function
 
+import pickle
 import argparse
 import json
+from textwrap import dedent
 
 import numpy as np
 
@@ -33,13 +35,19 @@ def parse_args():
                         help="""Add predictions of the model to the tuple. Note
                         that this is NOT done in a cross-validated manner.""")
     parser.add_argument('-s', '--save-model', type=str, default=None,
-                        help="""Write the XGBoost model to disk.""")
+                        help="""Write the XGBoost model to disk. This will
+                        store the XGBoost dump text file and a pickled version
+                        of the classifier to disk""")
     parser.add_argument('--stop', type=int, default=None,
                         help="""The read_root stop argument.""")
     parser.add_argument('--verbose', default=False, action='store_true',
                         help="""Don't be silent while training.""")
     parser.add_argument('--additional-selection', default='', type=str,
                         help="""Define an additional selection string""")
+    parser.add_argument('--print-stats', default=True, action='store_false',
+                        help="""Print some event numbers for the base tuple and
+                        the selected tuple, as well as the selection efficiency
+                        .""")
     return parser.parse_args()
 
 
@@ -50,11 +58,7 @@ def parse_config(filename):
         return json.load(f)
 
 
-def print_tp(df, args, config):
-    print('{:.2f}%'.format(100 * tagging_power_score(df, config, etas='probas')))
-
-
-def add_predictions(args, config):
+def read_dataset(args, config):
     print('Reading data...', end='', flush=True)
     df = read_root(args.input_file, stop=args.stop)
     df['target'] = df.eval(config['target_eval'])
@@ -64,7 +68,24 @@ def add_predictions(args, config):
     if args.additional_selection:
         df.query(args.additional_selection, inplace=True)
     print(' done.')
+    return df
 
+
+def print_statistics(df, args, config):
+    print('Calculating tuple statistics...', flush=True)
+    num_selected_events = df.SigYield_sw.sum()
+    num_selected_tagging_particles = len(df)
+    num_events = get_event_number(config)
+    efficiency = num_selected_events / num_events
+    # print some statistics about the dataset
+    print(f'Dataset contains {num_selected_events:.2f} (weighted) events with '
+          f'{num_selected_tagging_particles} tagging particles, out of '
+          f'{num_events:.2f}, corresponding to an efficiency of '
+          f'{100 * efficiency:.2f}%.')
+    return efficiency, num_selected_events, num_events
+
+
+def add_predictions(df, args, config):
     xgb_kwargs = config.get('xgb_kwargs', {})
     model = XGBClassifier(**xgb_kwargs, nthread=config.get('n_jobs'),
                           silent=not args.verbose)
@@ -81,9 +102,10 @@ def add_predictions(args, config):
     print(' done.')
 
     if args.save_model:
-        print('Saving model to `{}`...'.format(
+        print('Saving model to `{}.{{txt,pkl}}`...'.format(
             args.save_model), end='', flush=True)
-        model.booster().save_model(args.save_model)
+        model.booster().dump_model(args.save_model + '.txt')
+        pickle.dump(model, open(args.save_model + '.pkl', 'wb'))
         print(' done.')
 
     if args.output_file:
@@ -102,9 +124,16 @@ def main():
     if args.has_predictions:
         df = read_root(args.input_file)
     else:
-        df = add_predictions(args, config)
+        df_no_prediction = read_dataset(args, config)
+        df = add_predictions(df_no_prediction, args, config)
 
-    print_tp(df, args, config)
+    if args.print_stats:
+        efficiency, _, _ = print_statistics(df, args, config)
+    else:
+        efficiency = None
+    tagging_power = tagging_power_score(df, config, efficiency=efficiency,
+                                        etas='probas')
+    print(f'Tagging power (uncalibrated): {100 * tagging_power:.2f}%')
 
 
 if __name__ == '__main__':
